0729c3b9c5dc8f743844ace516118b8ec18fcb3e
Erica Siegel <siegel.erica@gmail.com>
2023-01-20 14:40:38 -0800

    fixed cascading, added choices to Baked class

--- bakery/models.py
+++ bakery/models.py
@@ -11,15 +11,29 @@ class Dough(models.Model):
     cookie = models.ForeignKey(Cookie, on_delete=models.CASCADE)
     
 class Baked(models.Model):
-    frozen_ct = models.IntegerField()
+    PACKAGED = 'P'
+    FROZEN = 'F'
+    
+    BAKED_CHOICES = [
+        (PACKAGED, 'Packaged'),
+        (FROZEN, 'Frozen')
+    ]
+    
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=1, choices=BAKED_CHOICES, default=FROZEN)
     location = models.CharField(max_length=255)
     date_baked = models.DateField(auto_now=True)
     cookie = models.ForeignKey(Cookie, on_delete=models.CASCADE)
+    
+# class Store(models.Model):
+#     last_updated = models.DateField(auto_now=False)
 
 class StoreCookie(models.Model):
     quantity = models.IntegerField()
-    cookie = models.ForeignKey(Cookie, on_delete=models.PROTECT, related_name='CookieInStore')
+    cookie = models.OneToOneField(Cookie, on_delete=models.CASCADE, related_name='CookieInStore')
     last_updated = models.DateField(auto_now=True)
+    # store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    
     
 
     
