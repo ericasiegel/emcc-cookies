48cfda49e42929e84a2ff4aeca9b442b4b0787ff
Erica Siegel <siegel.erica@gmail.com>
2023-01-30 11:25:08 -0800

    added Mini model

--- bakery/admin.py
+++ bakery/admin.py
@@ -8,8 +8,9 @@ from . import models
 
 @admin.register(models.Cookie)
 class CookieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'baked_cookie_par', 'baked_quantity', 'dough_par', 'dough_quantity', 'store_quantity']
+    list_display = ['name', 'baked_cookie_par', 'baked_quantity', 'mini_quantity', 'dough_par', 'dough_quantity', 'store_quantity']
     title = 'Cookie Name'
+    search_fields = ['name__icontains']
     
     def baked_quantity(self, cookie):
         url = (
@@ -21,9 +22,6 @@ class CookieAdmin(admin.ModelAdmin):
         )
         return format_html('<a href="{}">{}</a>', url, cookie.baked_quantity)
     
-    # def baked_quantity(self, cookie):
-    #     return cookie.baked_quantity
-    
     def dough_quantity(self, cookie):
         url = (
             reverse('admin:bakery_dough_changelist')
@@ -34,11 +32,24 @@ class CookieAdmin(admin.ModelAdmin):
         )
         return format_html('<a href="{}">{}</a>', url, cookie.dough_quantity)
     
+    def mini_quantity(self, cookie):
+        url = (
+            reverse('admin:bakery_mini_changelist')
+            + '?'
+            + urlencode({
+                'cookie__id':str(cookie.id)
+            })
+        )
+        return format_html('<a href="{}">{}</a>', url, cookie.mini_quantity)
+    
     def store_quantity(self, cookie):
         return cookie.store_quantity
-    
+
     def get_queryset(self, request):
-        return super().get_queryset(request).annotate(
+        queryset = super().get_queryset(request)
+        
+        return queryset.annotate(
+            mini_quantity = Sum('mini__quantity'),
             baked_quantity = Sum('baked__quantity'),
             dough_quantity = Sum('dough__quantity'),
             store_quantity = F('storecookie__quantity')
@@ -49,6 +60,18 @@ class CookieAdmin(admin.ModelAdmin):
 class BakedAdmin(admin.ModelAdmin):
     list_display = ['cookie_name', 'quantity', 'date_baked', 'status', 'location']
     list_select_related = ['cookie']
+    search_fields = ['cookie_name__icontains', 'location__icontains', 'status__icontains']
+    list_filter = ['status', 'location', 'date_baked']
+    
+    def cookie_name(self, baked):
+        return baked.cookie.name
+
+@admin.register(models.Mini)
+class MiniAdmin(admin.ModelAdmin):
+    list_display = ['cookie_name', 'quantity', 'date_baked', 'status', 'location']
+    list_select_related = ['cookie']
+    search_fields = ['cookie_name__icontains', 'location__icontains', 'status__icontains']
+    list_filter = ['status', 'location', 'date_baked']
     
     def cookie_name(self, baked):
         return baked.cookie.name
@@ -57,6 +80,7 @@ class BakedAdmin(admin.ModelAdmin):
 class DoughAdmin(admin.ModelAdmin):
     list_display = ['cookie_name', 'quantity', 'date_frozen', 'location']
     list_select_related = ['cookie']
+    search_fields = ['cookie__name__icontains', 'location__icontains']
     
     def cookie_name(self, dough):
         return dough.cookie.name
@@ -65,6 +89,7 @@ class DoughAdmin(admin.ModelAdmin):
 class StoreCookieAdmin(admin.ModelAdmin):
     list_display = ['cookie_name', 'par', 'quantity', 'last_updated']
     list_select_related = ['cookie']
+    search_fields = ['cookie__name__icontains']
     
     def cookie_name(self, baked):
         return baked.cookie.name
--- bakery/models.py
+++ bakery/models.py
@@ -1,5 +1,29 @@
 from django.db import models
 
+
+TOP_FREEZER = 'T'
+BOTTOM_FREEZER = 'B'
+REFRIGERATOR = 'R'
+STORAGE_FREEZER = 'SF'
+STORAGE_REFRIGERATOR = 'SR'
+
+    
+LOCATION_CHOICES = [
+    (TOP_FREEZER, 'Top Freezer'),
+    (BOTTOM_FREEZER, 'Bottom Freezer'),
+    (REFRIGERATOR, 'Refrigerator'),
+    (STORAGE_FREEZER, 'Storage Freezer'),
+    (STORAGE_REFRIGERATOR, 'Storage Refrigerator')
+]
+
+PACKAGED = 'P'
+FROZEN = 'F'
+    
+BAKED_CHOICES = [
+    (PACKAGED, 'Packaged'),
+    (FROZEN, 'Frozen')
+]
+
 # Create your models here.
 class Cookie(models.Model):
     name = models.CharField(max_length=255)
@@ -16,7 +40,7 @@ class Cookie(models.Model):
 
 class Dough(models.Model):
     quantity = models.IntegerField()
-    location = models.CharField(max_length=255)
+    location = models.CharField(max_length=2, choices=LOCATION_CHOICES, default=TOP_FREEZER)
     date_frozen = models.DateField(auto_now=True)
     cookie = models.ForeignKey(Cookie, on_delete=models.CASCADE)
     
@@ -25,24 +49,26 @@ class Dough(models.Model):
         verbose_name = 'Cookie Dough'
     
 class Baked(models.Model):
-    PACKAGED = 'P'
-    FROZEN = 'F'
-    
-    BAKED_CHOICES = [
-        (PACKAGED, 'Packaged'),
-        (FROZEN, 'Frozen')
-    ]
-    
     quantity = models.IntegerField()
     status = models.CharField(max_length=1, choices=BAKED_CHOICES, default=FROZEN)
-    location = models.CharField(max_length=255)
+    location = models.CharField(max_length=2, choices=LOCATION_CHOICES, default=TOP_FREEZER)
     date_baked = models.DateField(auto_now=True)
     cookie = models.ForeignKey(Cookie, on_delete=models.CASCADE)
     
     class Meta:
         ordering = ['cookie']
         verbose_name = 'Baked Cookie'
+
+class Mini(models.Model):
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=1, choices=BAKED_CHOICES, default=FROZEN)
+    location = models.CharField(max_length=2, choices=LOCATION_CHOICES, default=TOP_FREEZER)
+    date_baked = models.DateField(auto_now=True)
+    cookie = models.ForeignKey(Cookie, on_delete=models.CASCADE)
     
+    class Meta:
+        ordering = ['cookie']
+        verbose_name = 'Mini Cookie'
 
 class StoreCookie(models.Model):
     quantity = models.IntegerField()
